d32496cb53f8bbbbab6a4bcda2350eb0fa2b6bee
Pau Masip <pau.masip@enti.cat>
2018-05-08 13:06:12 +0200

    a

--- AA1_10/Main.cpp
+++ AA1_10/Main.cpp
@@ -1,5 +1,24 @@
+#include <iostream>
+
+void BubbleSort(int a[], int length)
+{
+	int temp;
+	for (int i = 0; i < length; ++i)
+	{
+		for (int j = 0; j < (length - 1); ++j)
+		{
+			if (a[j] > a[j + 1])
+			{
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+}
 
 int main()
 {
+	
 	return 0;
 }
